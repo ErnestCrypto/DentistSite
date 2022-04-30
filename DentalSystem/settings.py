a48a98b61dba2b3e900f54b08c8853ca25498da2
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-04-30 01:11:20 +0000

    commit

--- DentalSystem/settings.py
+++ DentalSystem/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import environ
 
 env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
