bdc81124ce43322905cb037e8abc7972db161a09
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-05-02 11:52:27 +0000

    commit

--- website/urls.py
+++ website/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('about/', views.about, name="about"),
     path('appointment/', views.appointment, name="appointment"),
     path('price/', views.price, name="price"),
-    path('service/', views.service, name="service"),
+    path('services/', views.service, name="service"),
     path('team/', views.team, name="team"),
     path('testimonial/', views.testimonial, name="testimonial"),
 
